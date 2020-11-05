2e81f0a30aac5136671031c77e33c98ab0bb9d87
smithwebtek <brad@smithwebtek.com>
2020-11-04 22:23:37 -0500

    Add staff page

--- globe_bank/public/staff/index.php
+++ globe_bank/public/staff/index.php
@@ -1,8 +1,31 @@
-<?php require('../../private/initialize.php'); ?>
+<!doctype html>
 
-<?php $page_title = 'Staff Menu' ?>
-<?php include('../../private/shared/staff_header.php'); ?>
+<html lang="en">
 
-<div id="content"></div>
+<head>
+  <title>GBI</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+  </link>
+</head>
 
-<?php include('../../private/shared/staff_footer.php'); ?>
+<body>
+  <header>
+    <h1>GBI Staff Area</h1>
+  </header>
+
+  <navigation>
+    <ul>
+      <li><a href="index.php">Menu</a></li>
+    </ul>
+  </navigation>
+
+  <div id="content">
+  </div>
+
+  <footer>
+    &copy; <?php echo date('Y'); ?> Globe Bank
+  </footer>
+</body>
+
+</html>
