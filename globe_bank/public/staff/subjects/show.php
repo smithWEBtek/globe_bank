d44d442bc0cfad04d82b7f3ede56b56e7bcbdbe4
smithwebtek <brad@smithwebtek.com>
2020-11-09 20:39:33 -0500

    HTMLspecialChars

--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -10,33 +10,9 @@
   // echo 'example of super global variable, page attribute cast as integer'
   // $page_as_int = (int) $_GET['page'];
   // echo gettype($page_as_int);
-
-
-
   $id = $_GET['id'];
-  
-  // if ($_GET['name'] == 0 ) {
-    // $name = 'Name not specified';
-  // } else if (isset($_GET['name'])) {
-    // $name = $_GET['name'];
-  // }
-
-  // function nameSet(arg) {
-    // $name = isset($_GET[arg]) ? $_GET[arg] : 'Not specfied';
-  // }
-
-  // $name = ( (null != $_GET['name']) && ($_GET['name'] != '0') ) ? $_GET['name'] : 'Name not specified')
-
-
-  // $name = $_GET['name'] == '0' ? 'Name not specified' 
-  // $name = isset($_GET['name']) ? $_GET['name'] : 'Name not specified';
-
-
-  $name = $_GET['name']; 
-
-
-  
-  ?>
+  $name = $_GET['name'];
+?>
 <h1>Subject: Show page for ID: <?php echo $id ?></h1>
 <p>Name variable from URL:
 <h3><?php echo $name ?></h3>
