233e4d218918a86cf6b9ba84be86df2adcabd543
brad@smithwebtek.com <brad@smithwebtek.com>
2021-01-27 00:15:46 -0500

    Through end of section 01

--- globe_bank/public/staff/subjects/index.php
+++ globe_bank/public/staff/subjects/index.php
@@ -1,61 +1,11 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
-<?php
-  $subjects = [
-    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About Globe Bank'],
-    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Consumer'],
-    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Small Business'],
-    ['id' => '4', 'position' => '4', 'visible' => '0', 'menu_name' => 'Commercial'],
-    ['id' => '5', 'position' => '5', 'visible' => '1', 'menu_name' => 'Investors'],
-    ['id' => '6', 'position' => '6', 'visible' => '1'],
-  ];
-?>
-
 <?php $page_title = 'Subjects'; ?>
+
+<!-- <?php include('../../../private/shared/staff_header.php'); ?> -->
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-<div id="content">
-  <div class="subjects listing">
-    <h1>Subjects</h1>
+<div id="content"></div>
 
-    <div class="actions">
-      <a class="action" href="">Create New: Subject</a>
-    </div>
-
-    <table class="list">
-      <tr>
-        <th>ID</th>
-        <th>Position</th>
-        <th>Visible</th>
-        <th>Name</th>
-        <th>&nbsp;</th>
-        <th>&nbsp;</th>
-        <th>&nbsp;</th>
-      </tr>
-
-      <?php foreach($subjects as $subject) { 
-        $name = 'Not specified'; 
-        if(isset($subject['menu_name'])) {
-          $name = $subject['menu_name'];
-        }
-      ?>
-
-      <tr>
-        <td><?php echo h($subject['id']); ?></td>
-        <td><?php echo h($subject['position']); ?></td>
-        <td><?php echo $subject['visible'] == 1 ? 'true' : 'false'; ?></td>
-        <td><?php echo $name ?></td>
-        <td><a class="action"
-            href="<?php echo url_for('/staff/subjects/show.php?id=' . $subject['id'] . '&name=' . $name); ?>">View</a>
-        </td>
-        <td><a class="action" href="">Edit</a></td>
-        <td><a class="action" href="">Delete</a></td>
-      </tr>
-      <?php } ?>
-    </table>
-
-  </div>
-
-</div>
-
+<!-- <?php include('../../../private/shared/staff_footer.php'); ?> -->
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>
